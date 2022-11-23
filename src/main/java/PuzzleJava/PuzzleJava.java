78f37112c4a2c2af6c11f9ee2c6035d1c3910247
ZhouSusan <szhou089@gmail.com>
2022-11-22 23:31:12 -0800

    filterNameList() and printAllElementsOfArray() in PuzzleJava.java

--- src/main/java/PuzzleJava/PuzzleJava.java
+++ src/main/java/PuzzleJava/PuzzleJava.java
@@ -1,5 +1,7 @@
 package PuzzleJava;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class PuzzleJava {
 
@@ -15,13 +17,38 @@ public class PuzzleJava {
         return nums;
     }
 
+    public static ArrayList<String> filterNameList(String[] arr) {
+
+        ArrayList<String> filteredNameList = new ArrayList<String>();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].length() > 5){
+                filteredNameList.add(arr[i]);
+            }
+        }
+        printAllElementsOfArrayList(filteredNameList);
+        return filteredNameList;
+    }
+
     public static void printAllElementsOfArray(int[] arr) {
         for (int j = 0; j < arr.length; j++) {
             System.out.println(arr[j]);
         }
     }
+
+    public static void printAllElementsOfArrayList(ArrayList<String> list) {
+        for (String x : list) {
+            System.out.println(x);
+        }
+    }
     public static void main(String[] args) {
         int [] testCaseOne = {3,5,1,2,7,9,8,13,25,32};
         filteredArray(testCaseOne);
+
+        System.out.println("####################################");
+        String[] namesList = {"Nancy", "Jinichi", "Fujibayashi", "Momochi", "Ishikawa"};
+        filterNameList(namesList);
+
+        System.out.println("####################################");
+        
     }
 }
